a256690e76d5f3c52a2fb4d6f39925a9c4cd19cc
Alchyr <isithalchyr@gmail.com>
2026-03-19 13:35:56 -0700

    new template

---
+++ content/ContentModTemplate/ContentModCode/Powers/ContentModPower.cs
@@ -0,0 +1,28 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Extensions;
+using ContentMod.ContentModCode.Extensions;
+using Godot;
+
+namespace ContentMod.ContentModCode.Powers;
+
+public abstract class ContentModPower : CustomPowerModel
+{
+    //Loads from ContentMod/images/powers/your_power.png
+    public override string CustomPackedIconPath
+    {
+        get
+        {
+            var path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".PowerImagePath();
+            return ResourceLoader.Exists(path) ? path : "power.png".PowerImagePath();
+        }
+    }
+
+    public override string CustomBigIconPath
+    {
+        get
+        {
+            var path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigPowerImagePath();
+            return ResourceLoader.Exists(path) ? path : "power.png".BigPowerImagePath();
+        }
+    }
+}
---
+++ content/ContentModTemplate/ContentModCode/Relics/ContentModRelic.cs
@@ -0,0 +1,37 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Extensions;
+using ContentMod.ContentModCode.Extensions;
+using Godot;
+
+namespace ContentMod.ContentModCode.Relics;
+
+public abstract class ContentModRelic : CustomRelicModel
+{
+    //ContentMod/images/relics
+    public override string PackedIconPath
+    {
+        get
+        {
+            var path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".RelicImagePath();
+            return ResourceLoader.Exists(path) ? path : "relic.png".RelicImagePath();
+        }
+    }
+
+    protected override string PackedIconOutlinePath
+    {
+        get
+        {
+            var path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}_outline.png".RelicImagePath();
+            return ResourceLoader.Exists(path) ? path : "relic_outline.png".RelicImagePath();
+        }
+    }
+
+    protected override string BigIconPath
+    {
+        get
+        {
+            var path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigRelicImagePath();
+            return ResourceLoader.Exists(path) ? path : "relic.png".BigRelicImagePath();
+        }
+    }
+}
